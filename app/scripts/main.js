65cf65f35fe93b4da25b99f639ad24ca97ae783e
Oliver Stenkilde <oliver@stenkilde.com>
2014-02-06 16:13:17 +0100

    INIT COMMIT

---
+++ app/scripts/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$(document).foundation();
+	$(document).ready(function() {
+		$(".trigger").click(function(){
+			$(".nav-off-canv, .canv").toggleClass("js-is-open");
+		});
+	});
+	
+	$.ionSound({
+		sounds: [
+		"ba_dumm"
+		],
+		path: "sounds/",
+		multiPlay: false,
+		volume: "1.0"
+	});
+
+	$("#badumm").on("click", function(){
+		$('.playbutton').fadeOut().delay(3000).fadeIn();
+		$('.wave').addClass('animate')
+		.delay(3000)
+		.queue(function() {
+			$(this).removeClass('animate');
+			$(this).dequeue();
+		});;
+		$.ionSound.play("ba_dumm");
+	});
+});
